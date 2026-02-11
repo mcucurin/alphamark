453099e5ff140ebeb3262c1ed7eae91d7388957f
Haoran Li <lihaoran@g.ucla.edu>
2026-02-11 11:33:41 -0800

    Removed excess files

--- pkl.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read CSV
-df = pd.read_csv("/Users/dhruvpatel/financial_pipeline/test_input/features_20250102.csv")
-
-# Save as pickle
-df.to_pickle("features_20250102.pkl")
